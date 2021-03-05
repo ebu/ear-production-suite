4e5dc857d67f0bb05ecb31afc6bb46b9e721477d
jonas <jonas.merkt@impulse-audio-lab.com>
2021-03-05 18:07:29 +0100

    Update REAPER api and implement GetSetProjectInfo function. Use this function to determine tail length when rendering.

--- reaper-adm-extension/src/reaper_adm/reaperapi.h
+++ reaper-adm-extension/src/reaper_adm/reaperapi.h
@@ -151,6 +151,7 @@ class ReaperAPI {
     virtual double GetProjectLength(ReaProject* proj) const = 0;
     virtual int GetTrackNumMediaItems(MediaTrack* tr) const = 0;
     virtual MediaItem* GetTrackMediaItem(MediaTrack* tr, int itemidx) const = 0;
+    virtual double GetSetProjectInfo(ReaProject * project, const char* desc, double value, bool is_set) const = 0;
 
     static constexpr int RENDER_ITEMS_AS_NEW_TAKE_ID = 41999;
     static constexpr int CROP_TO_ACTIVE_TAKE_IN_ITEMS = 40131;
