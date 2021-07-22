d9533dec0670c8f3d5aead052c8f2f098b250537
marthellio <martha.elliott@bbc.co.uk>
2021-07-22 16:04:30 +0100

    fixes to channel count in HOA pushBearMetadata

--- ear-production-suite-plugins/lib/src/binaural_monitoring_audio_processor.cpp
+++ ear-production-suite-plugins/lib/src/binaural_monitoring_audio_processor.cpp
@@ -145,8 +145,8 @@ bool BinauralMonitoringAudioProcessor::pushBearMetadata(
   bearMetadata.channels.reserve(metadata->degrees.size());
 
   for (int i = 0; i < metadata->degrees.size(); i++) {
-    bearMetadata.channels.push_back(channelNum + i);
     hoaChannelMappings.push_back(channelNum + i);
+    bearMetadata.channels.push_back(hoaChannelMappings.size() -1);
   }
 
   bearMetadata.rtime = metadataRtime;
