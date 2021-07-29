e542e835a4ce5804bdb95b1ad67ec6269154c74c
Matt Firth <matt.firth@bbc.co.uk>
2021-07-29 16:30:04 +0100

    Element Container add/move/remove methods and Listeners

--- ear-production-suite/plugins/scene/src/elements_container.cpp
+++ ear-production-suite/plugins/scene/src/elements_container.cpp
@@ -2,6 +2,8 @@
 
 #include "elements_container.hpp"
 
+#include "helper/move.hpp"
+
 namespace ear {
 namespace plugin {
 namespace ui {
@@ -24,6 +26,54 @@ void ElementsContainer::resized() {
   auto area = getLocalBounds();
   viewport->setBounds(area.reduced(2, 2));
   list->setBounds(area.reduced(2, 2));
+}
+
+void ElementsContainer::removeElement(ElementView* element) {
+  auto it = std::find_if(
+    elements.begin(), elements.end(),
+    [element](auto candidate) { return candidate.get() == element; });
+  auto index = std::distance(elements.begin(), it);
+  elements.erase(it);
+  list->removeChildComponent(element);
+  list->resized();
+  Component::BailOutChecker checker(this);
+  listeners_.callChecked(checker, [this, index](Listener& l) {
+    l.removeElementClicked(this->list.get(), index);
+  });
+  if (checker.shouldBailOut()) {
+    return;
+  }
+}
+
+void ElementsContainer::moveElement(int oldIndex, int newIndex) {
+  if (oldIndex < elements.size() && newIndex < elements.size() &&
+      oldIndex != newIndex) {
+    move(elements.begin(), oldIndex, newIndex);
+  }
+  Component::BailOutChecker checker(this);
+  listeners_.callChecked(checker, [this, oldIndex, newIndex](Listener& l) {
+    l.elementMoved(this->list.get(), oldIndex, newIndex);
+  });
+  if (checker.shouldBailOut()) {
+    return;
+  }
+}
+
+void ElementsContainer::addElement(std::shared_ptr<ElementView> element) {
+  element->getRemoveButton()->onClick = [this, element]() {
+    removeElement(element.get());
+  };
+  elements.push_back(element);
+  list->addAndMakeVisible(element.get());
+  resized();
+}
+
+void ElementsContainer::addListener(Listener* l) {
+  listeners_.add(l);
+}
+
+void ElementsContainer::removeListener(Listener* l) {
+  listeners_.remove(l);
 }
 
 }  // namespace ui
--- ear-production-suite/plugins/scene/src/elements_container.hpp
+++ ear-production-suite/plugins/scene/src/elements_container.hpp
@@ -19,11 +19,28 @@ class ElementsContainer : public Component {
 
   void resized() override;
 
+  void addElement(std::shared_ptr<ElementView> element);
+  void removeElement(ElementView* element);
+  void moveElement(int oldIndex, int newIndex);
+
+  class Listener {
+   public:
+    virtual ~Listener() = default;
+
+    virtual void elementMoved(ElementViewList* list, int oldIndex,
+                              int newIndex) = 0;
+    virtual void removeElementClicked(ElementViewList* list, int index) = 0;
+  };
+
+  void addListener(Listener* l);
+  void removeListener(Listener* l);
+
   std::unique_ptr<Viewport> viewport;
   std::unique_ptr<ElementViewList> list;
   std::vector<std::shared_ptr<ElementView>> elements;
 
  private:
+  ListenerList<Listener> listeners_;
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ElementsContainer)
 };
 
