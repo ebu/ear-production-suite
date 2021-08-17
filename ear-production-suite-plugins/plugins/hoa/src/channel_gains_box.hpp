b1c064f1ee1daa0dadca19f1649c3ffbbe851d2d
marthellio <martha.elliott@bbc.co.uk>
2021-08-17 12:17:55 +0100

    experimenting with pyramid layout in HOA plugin UI

--- ear-production-suite-plugins/plugins/hoa/src/channel_gains_box.hpp
+++ ear-production-suite-plugins/plugins/hoa/src/channel_gains_box.hpp
@@ -41,11 +41,28 @@ class ChannelGainsBox : public Component {
 
   void updateChannelGainBounds() {
     auto area = getLocalBounds();
-    area.removeFromBottom(350);//ME add
-    for (auto channelGain : channelGains_) {
+
+    //ME add
+    int levels(ceil(sqrt(channelGains_.size())));
+    double levelMeterSize(area.getHeight() /
+                          static_cast<double>(levels));  // ME experiment
+    // for (auto channelGain : channelGains_) {
+    for (int i(0); i < channelGains_.size(); i++) {  // ME experiment
+      int level(ceil(sqrt(i + 1)));
+      auto channelGain(channelGains_[i]);  // ME experiment
+      channelGain->setBounds(
+          area.withLeft(50 * (i - pow(level - 1, 2)))
+              .withRight((50 * (i - pow(level - 1, 2))) + 50)
+              .withTrimmedTop((level - 1) * levelMeterSize)
+              .withTrimmedBottom((levels - level) * levelMeterSize));
+      area.removeFromLeft(6);  // NEEDS WORK
+    }
+      //ME end
+    // area.removeFromBottom(350);
+    /*for (auto channelGain : channelGains_) {
       channelGain->setBounds(area.removeFromLeft(50));
       area.removeFromLeft(6);
-    }
+    }*/
   }
 
   void addChannelGain(ChannelGain* channelGain) {
