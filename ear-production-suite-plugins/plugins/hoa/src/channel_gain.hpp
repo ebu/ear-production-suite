42a101b8ad3f2d70eb751577ef4970e5b5fea312
marthellio <martha.elliott@bbc.co.uk>
2021-08-17 12:12:17 +0100

    removed gainslider

--- ear-production-suite-plugins/plugins/hoa/src/channel_gain.hpp
+++ ear-production-suite-plugins/plugins/hoa/src/channel_gain.hpp
@@ -46,7 +46,7 @@ class ChannelGain : public Component {
           .retainCharacters("0123456789.,-+")
           .getDoubleValue();
     };
-    gainSlider_->setSliderStyle(Slider::LinearVertical);
+    /*gainSlider_->setSliderStyle(Slider::LinearVertical);
     gainSlider_->setTextBoxStyle(Slider::TextBoxAbove, false, 44, 40);
     gainSlider_->setRange(-100.f, 6.f);
     gainSlider_->setNumDecimalPlacesToDisplay(1);
@@ -54,7 +54,7 @@ class ChannelGain : public Component {
     gainSlider_->setSkewFactorFromMidPoint(-20.f);
     gainSlider_->setEnabled(false);
     gainSlider_->setAlpha(Emphasis::disabled);
-    addAndMakeVisible(gainSlider_.get());
+    addAndMakeVisible(gainSlider_.get());*/
   }
   ~ChannelGain() {}
 
@@ -62,11 +62,11 @@ class ChannelGain : public Component {
 
   void resized() override {
     auto area = getLocalBounds();
-    area.removeFromTop(5);
+    area.removeFromTop(5).removeFromBottom(100);//ME change
     levelMeter_->setBounds(
         area.removeFromTop(120).withSizeKeepingCentre(13, 120));
     speakerLabel_->setBounds(area.removeFromTop(30));
-    gainSlider_->setBounds(area);
+    //gainSlider_->setBounds(area);
   }
 
   EarSlider* getGainSlider() { return gainSlider_.get(); }
