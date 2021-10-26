08227d308812d8ace83998b38632dabfe872df04
marthellio <martha.elliott@bbc.co.uk>
2021-10-26 12:29:17 +0100

    Added correct number of pyramid boxes to each order box

--- ear-production-suite-plugins/plugins/hoa/src/order_box.hpp
+++ ear-production-suite-plugins/plugins/hoa/src/order_box.hpp
@@ -6,6 +6,7 @@
 #include "components/look_and_feel/fonts.hpp"
 #include "level_display_box.hpp"
 #include "pyramid_box.hpp"
+#include <math.h>
 
 namespace ear {
 namespace plugin {
@@ -13,11 +14,11 @@ namespace ui {
 
 class OrderBox : public Component {
  public:
-  OrderBox(String name, int order)
+  OrderBox(String name, int rowOrder)
       : //levelMeter_(std::make_unique<LevelMeter>()),
         orderLabel_(std::make_unique<Label>()),
         levelDisplayBox_(std::make_unique<LevelDisplayBox>()),
-        order_(order)  {
+        order_(rowOrder) {
     //levelMeter_->setOrientation(LevelMeter::vertical);
     //addAndMakeVisible(levelMeter_.get());
 
@@ -27,8 +28,15 @@ class OrderBox : public Component {
     orderLabel_->setJustificationType(Justification::centred);
     addAndMakeVisible(orderLabel_.get());
 
-    for (int i(0); i < order;i++) {
-      PyramidBox pyramidBox = PyramidBox(std::to_string(i + order));
+    auto numChannelsOnRow = [](int value) { 
+      return value < 0 ? 0 : value * 2 + 1; 
+    };
+    auto numChannelsInOrder = [](int value) {
+      return pow(value + 1, 2);
+    };
+
+    for (int i(0); i < numChannelsOnRow(rowOrder); i++) {
+      PyramidBox pyramidBox = PyramidBox(std::to_string(i + 1 +  numChannelsInOrder(rowOrder - 1)));
       pyramidBoxes_.push_back(&pyramidBox);
     }
   }
@@ -53,7 +61,7 @@ class OrderBox : public Component {
   void updatePyramidBoxBounds() {  // This seems to be where we place the
                                     // channel gain boxes
     auto area = getLocalBounds();
-    for (auto pyramidBox : pyramidBoxes_) {
+    for (auto *pyramidBox : pyramidBoxes_) {
       area.removeFromLeft(getLocalBounds().getWidth() / pyramidBoxes_.size());
       pyramidBox->setBounds(area.removeFromLeft(50));
       area.removeFromLeft(6);
