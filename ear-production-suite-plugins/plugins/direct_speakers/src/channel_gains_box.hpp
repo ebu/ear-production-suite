833dd60333d58c0b0aa0074f5025f6ad00b714a0
Richard Bailey <rsjtaylor.github@gmail.com>
2021-11-10 11:27:48 +0000

    remove USE_NEW_DESIGN ifdefs and just use the new design

--- ear-production-suite-plugins/plugins/direct_speakers/src/channel_gains_box.hpp
+++ ear-production-suite-plugins/plugins/direct_speakers/src/channel_gains_box.hpp
@@ -18,7 +18,6 @@ class ChannelGainsBox : public Component {
   void paint(Graphics& g) override {
     auto area = getLocalBounds();
     if (channelGains_.empty()) {
-#ifdef USE_NEW_DESIGN
       area.reduce(20.f, 30.f);
 
       g.setColour(EarColours::Text.withAlpha(Emphasis::medium));
@@ -29,24 +28,6 @@ class ChannelGainsBox : public Component {
       g.setFont(EarFonts::Heading);
       g.drawText("Please select a speaker layout first",
                  area.removeFromTop(25.f), Justification::left);
-#else
-      g.fillAll(EarColours::Area01dp);
-      float lineThickness = 1.f;
-      g.setColour(EarColours::Error);
-      g.drawRect(
-          area.toFloat().reduced(lineThickness / 2.f, lineThickness / 2.f),
-          lineThickness);
-      area.reduce(20.f, 30.f);
-
-      g.setColour(EarColours::Text.withAlpha(Emphasis::medium));
-      g.setFont(EarFonts::Label);
-      g.drawText("Channel gains not available", area.removeFromTop(25.f),
-                 Justification::left);
-      g.setColour(EarColours::Text.withAlpha(Emphasis::high));
-      g.setFont(EarFonts::Heading);
-      g.drawText("Please select a speaker layout first",
-                 area.removeFromTop(25.f), Justification::left);
-#endif
     }
   }
 
