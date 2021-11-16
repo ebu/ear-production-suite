e8ab083e69b8817ece8862c91f4da0f682d8b68b
Matt Firth <matt.firth@bbc.co.uk>
2021-11-16 14:05:07 +0000

    Replace missing AT and AS in 2076-2 structures

--- reaper-adm-extension/src/reaper_adm/admmetadata.cpp
+++ reaper-adm-extension/src/reaper_adm/admmetadata.cpp
@@ -7,6 +7,30 @@
 using namespace admplug;
 
 namespace {
+
+void setUidReference(adm::Document& doc,
+                     adm::AudioTrackUid& uid,
+                     adm::AudioChannelFormatId elementId) {
+    // Specific function to provide 2076-2 structure support even though we're only supporting -1 atm
+    /*
+    When a CHNA chunk of a 2076-2 file is processed, we may find references to ChannelFormat elements
+     where we would normally expect to see a TrackFormat reference.
+    This is because, for PCM audio, the TrackFormat and StreamFormat elements are largely redundant,
+     so 2076-2 allows us to omit those elements and reference the ChannelFormat directly.
+    Because the rest of the EPS has been built with 2076-1 references and expected structures in mind,
+     it is easiest to workaround this issue by filling in these missing elements,
+     thus converting to a 2076-1-like structure.
+    */
+    auto cf = doc.lookup(elementId);
+    if(cf) {
+        auto tf = adm::AudioTrackFormat::create(adm::AudioTrackFormatName("AudioTrackFormat"), adm::FormatDefinition::PCM);
+        auto sf = adm::AudioStreamFormat::create(adm::AudioStreamFormatName("AudioStreamFormat"), adm::FormatDefinition::PCM);
+        uid.setReference(tf);
+        tf->setReference(sf);
+        sf->setReference(cf);
+    }
+}
+
 template <typename T>
 void setUidReference(adm::Document& doc,
                      adm::AudioTrackUid& uid,
@@ -14,6 +38,26 @@ void setUidReference(adm::Document& doc,
     auto element = doc.lookup(elementId);
     if(element) {
         uid.setReference(element);
+    }
+}
+
+void setUidReferenceUsingIdStr(adm::Document& doc,
+                            adm::AudioTrackUid& uid,
+                            std::string& elementIdStr) {
+    if(elementIdStr.rfind("AT_", 0) == 0) {
+        auto trackFormatId = adm::parseAudioTrackFormatId(elementIdStr);
+        setUidReference(doc, uid, trackFormatId);
+    } else if(elementIdStr.rfind("AC_", 0) == 0) {
+        std::string cfIdStr = elementIdStr.substr(0, 11); // counter portion does not apply to channelformat ID's
+        auto channelFormatId = adm::parseAudioChannelFormatId(cfIdStr);
+        setUidReference(doc, uid, channelFormatId);
+    } else if(elementIdStr.rfind("AP_", 0) == 0) {
+        auto packFormatId = adm::parseAudioPackFormatId(elementIdStr);
+        setUidReference(doc, uid, packFormatId);
+    } else {
+        auto msg = std::string("Unexpected ID: ");
+        msg += elementIdStr;
+        throw std::runtime_error(msg);
     }
 }
 
@@ -41,13 +85,11 @@ void ADMMetaData::completeUidReferences()
         auto admUid = document->lookup(uidId);
         if(admUid) {
             try {
-                auto trackFormatId = adm::parseAudioTrackFormatId(id.trackRef());
-                setUidReference(*document, *admUid, trackFormatId);
-                auto packFormatId = adm::parseAudioPackFormatId(id.packRef());
-                setUidReference(*document, *admUid, packFormatId);
-            } catch (std::runtime_error const& e) {
+                setUidReferenceUsingIdStr(*document, *admUid, id.trackRef()); // note could be channelformat
+                setUidReferenceUsingIdStr(*document, *admUid, id.packRef());
+            } catch(std::runtime_error const& e) {
                 // Prepend CHNA to error messages so we know the issue is related to CHNA, not AXML
-                auto msg = std::string("CHNA: ");
+                std::string msg("CHNA: ");
                 msg += e.what();
                 throw std::runtime_error(msg);
             }
