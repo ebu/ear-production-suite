d310e80ee9addbe3a5c6674c1be801c2f676cfde
Matt Firth <matt.firth@bbc.co.uk>
2021-11-17 19:51:39 +0000

    Run clang format on level_meter_calculator

--- shared/components/level_meter_calculator.cpp
+++ shared/components/level_meter_calculator.cpp
@@ -33,106 +33,102 @@ static float SIGNAL_PRESENCE_THRESHOLD = 0.00005;
 static float SIGNAL_CLIPPED_THRESHOLD = 1.0;
 
 LevelMeterCalculator::LevelMeterCalculator(std::size_t channels,
-	std::size_t samplerate)
-	: lastMeasurement_(juce::Time::currentTimeMillis()) {
-	setup(channels, samplerate);
+                                           std::size_t samplerate)
+    : lastMeasurement_(juce::Time::currentTimeMillis()) {
+    setup(channels, samplerate);
 }
 
 void LevelMeterCalculator::setup(std::size_t channels, std::size_t samplerate) {
-	std::lock_guard<std::mutex> lock(mutex_);
-	channels_ = channels;
-	samplerate_ = samplerate;
-	lastLevel_.clear();
-	lastLevel_.assign(channels_, 0.);
-	lastLevelHasSignal_.clear();
-	lastLevelHasSignal_.assign(channels_, false);
-	lastLevelHasClipped_.clear();
-	lastLevelHasClipped_.assign(channels_, false);
-	setConstants();
+    std::lock_guard<std::mutex> lock(mutex_);
+    channels_ = channels;
+    samplerate_ = samplerate;
+    lastLevel_.clear();
+    lastLevel_.assign(channels_, 0.);
+    lastLevelHasSignal_.clear();
+    lastLevelHasSignal_.assign(channels_, false);
+    lastLevelHasClipped_.clear();
+    lastLevelHasClipped_.assign(channels_, false);
+    setConstants();
 }
 
 void LevelMeterCalculator::process(const AudioBuffer<float>& buffer) {
-	std::lock_guard<std::mutex> lock(mutex_);
-	lastMeasurement_ = juce::Time::currentTimeMillis();
+    std::lock_guard<std::mutex> lock(mutex_);
+    lastMeasurement_ = juce::Time::currentTimeMillis();
     if(blocksize_ != buffer.getNumSamples()) {
         blocksize_ = buffer.getNumSamples();
         blockPeriodLimitMs_ = ((static_cast<float>(blocksize_) / static_cast<float>(samplerate_)) * 1000.f) * 2.f; // double expected
     }
-    for (std::size_t c = 0; c < channels_; ++c) {
-		bool hasSignal(false);
-		bool hasClipped(false);
-		for (std::size_t n = 0; n < buffer.getNumSamples(); ++n) {
-			auto sample(buffer.getSample(c, n));
+    for(std::size_t c = 0; c < channels_; ++c) {
+        bool hasSignal(false);
+        bool hasClipped(false);
+        for(std::size_t n = 0; n < buffer.getNumSamples(); ++n) {
+            auto sample(buffer.getSample(c, n));
             auto absSample = std::abs(sample);
-			processSample(sample, c);
-			if (absSample > SIGNAL_PRESENCE_THRESHOLD) {
+            processSample(sample, c);
+            if(absSample > SIGNAL_PRESENCE_THRESHOLD) {
                 hasSignal = true;
             }
-			if (absSample > SIGNAL_CLIPPED_THRESHOLD || lastLevelHasClipped_[c] == true) {
+            if(absSample > SIGNAL_CLIPPED_THRESHOLD || lastLevelHasClipped_[c] == true) {
                 hasClipped = true;
             }
-		}
-		lastLevelHasSignal_[c] = hasSignal;
-		lastLevelHasClipped_[c] = hasClipped;
-	}
+        }
+        lastLevelHasSignal_[c] = hasSignal;
+        lastLevelHasClipped_[c] = hasClipped;
+    }
 }
 
 bool LevelMeterCalculator::hasSignal(int channel) {
     std::lock_guard<std::mutex> lock(mutex_);
-	if (channel >= 0 && channel < lastLevelHasSignal_.size()) {
-		return lastLevelHasSignal_[channel];
-	}
-	else {
-		return false;
-	}
+    if(channel >= 0 && channel < lastLevelHasSignal_.size()) {
+        return lastLevelHasSignal_[channel];
+    } else {
+        return false;
+    }
 }
 
 bool LevelMeterCalculator::thisTrackHasClipped() {
     std::lock_guard<std::mutex> lock(mutex_);
-	for (size_t i(0); i < channels_; i++) {
-		if (lastLevelHasClipped_[i]) {
-			return true;
-		}
-	}
-	return false;
+    for(size_t i(0); i < channels_; i++) {
+        if(lastLevelHasClipped_[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 bool LevelMeterCalculator::thisChannelHasClipped(int channel) {
     std::lock_guard<std::mutex> lock(mutex_);
-	if (lastLevelHasClipped_[channel]) {
-		return true;
-	}
-	else {
-		return false;
-	}
+    if(lastLevelHasClipped_[channel]) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 float LevelMeterCalculator::getLevel(std::size_t channel) {
-	const std::lock_guard<std::mutex> lock(mutex_);
-	if (channel >= 0 && channel < lastLevel_.size()) {
-		return lastLevel_[channel];
-	}
-	else {
-		return 0.f;
-	}
+    const std::lock_guard<std::mutex> lock(mutex_);
+    if(channel >= 0 && channel < lastLevel_.size()) {
+        return lastLevel_[channel];
+    } else {
+        return 0.f;
+    }
 }
 
 void LevelMeterCalculator::processSample(float currentValue,
-	std::size_t channel) {
-	auto currentValueAbs = std::abs(currentValue);
-	if (currentValueAbs > lastLevel_[channel]) {
-		lastLevel_[channel] =
-			currentValueAbs - attack_constant_ * (currentValueAbs - lastLevel_[channel]);
-	}
-	else {
-		lastLevel_[channel] = lastLevel_[channel] * release_constant_;
-	}
+                                         std::size_t channel) {
+    auto currentValueAbs = std::abs(currentValue);
+    if(currentValueAbs > lastLevel_[channel]) {
+        lastLevel_[channel] =
+            currentValueAbs - attack_constant_ * (currentValueAbs - lastLevel_[channel]);
+    } else {
+        lastLevel_[channel] = lastLevel_[channel] * release_constant_;
+    }
 }
 
 void LevelMeterCalculator::decayIfNeeded() {
-	std::lock_guard<std::mutex> lock(mutex_);
-	juce::int64 time = juce::Time::currentTimeMillis();
-	auto duration = time - lastMeasurement_;
+    std::lock_guard<std::mutex> lock(mutex_);
+    juce::int64 time = juce::Time::currentTimeMillis();
+    auto duration = time - lastMeasurement_;
     if(duration >= blockPeriodLimitMs_) {
         // If here, we've not received any audio in expected period
         lastMeasurement_ = time;
@@ -152,99 +148,98 @@ void LevelMeterCalculator::decayIfNeeded() {
 
 void LevelMeterCalculator::resetClipping() {
     std::lock_guard<std::mutex> lock(mutex_);
-	for (std::size_t c = 0; c < channels_; ++c) {
-		lastLevelHasClipped_[c] = false;
-	}
+    for(std::size_t c = 0; c < channels_; ++c) {
+        lastLevelHasClipped_[c] = false;
+    }
 }
 
 void LevelMeterCalculator::setConstants() {
-	switch (samplerate_) {
-	case 8000:
-		attack_constant_ = ATTACK_8000;
-		release_constant_ = RELEASE_8000;
-		break;
-	case 11025:
-		attack_constant_ = ATTACK_11025;
-		release_constant_ = RELEASE_11025;
-		break;
-	case 22050:
-		attack_constant_ = ATTACK_22050;
-		release_constant_ = RELEASE_22050;
-		break;
-	case 44100:
-		attack_constant_ = ATTACK_44100;
-		release_constant_ = RELEASE_44100;
-		break;
-	case 48000:
-		attack_constant_ = ATTACK_48000;
-		release_constant_ = RELEASE_48000;
-		break;
-	case 88200:
-		attack_constant_ = ATTACK_88200;
-		release_constant_ = RELEASE_88200;
-		break;
-	case 96000:
-		attack_constant_ = ATTACK_96000;
-		release_constant_ = RELEASE_96000;
-		break;
-	case 192000:
-		attack_constant_ = ATTACK_192000;
-		release_constant_ = RELEASE_192000;
-		break;
-	case 384000:
-		attack_constant_ = ATTACK_384000;
-		release_constant_ = RELEASE_384000;
-		break;
-	default:
-		return calcConstants();
-	}
+    switch(samplerate_) {
+        case 8000:
+            attack_constant_ = ATTACK_8000;
+            release_constant_ = RELEASE_8000;
+            break;
+        case 11025:
+            attack_constant_ = ATTACK_11025;
+            release_constant_ = RELEASE_11025;
+            break;
+        case 22050:
+            attack_constant_ = ATTACK_22050;
+            release_constant_ = RELEASE_22050;
+            break;
+        case 44100:
+            attack_constant_ = ATTACK_44100;
+            release_constant_ = RELEASE_44100;
+            break;
+        case 48000:
+            attack_constant_ = ATTACK_48000;
+            release_constant_ = RELEASE_48000;
+            break;
+        case 88200:
+            attack_constant_ = ATTACK_88200;
+            release_constant_ = RELEASE_88200;
+            break;
+        case 96000:
+            attack_constant_ = ATTACK_96000;
+            release_constant_ = RELEASE_96000;
+            break;
+        case 192000:
+            attack_constant_ = ATTACK_192000;
+            release_constant_ = RELEASE_192000;
+            break;
+        case 384000:
+            attack_constant_ = ATTACK_384000;
+            release_constant_ = RELEASE_384000;
+            break;
+        default:
+            return calcConstants();
+    }
 }
 
 void LevelMeterCalculator::calcConstants() {
-	if (samplerate_ <= 0 || 384000 < samplerate_) {
-		attack_constant_ = 0.f;
-		release_constant_ = 0.f;
-		return;
-	}
+    if(samplerate_ <= 0 || 384000 < samplerate_) {
+        attack_constant_ = 0.f;
+        release_constant_ = 0.f;
+        return;
+    }
 
-	const float ATTACK_TIME = 5; /* milliseconds */
-	const float ATTACK_THRESHOLD = -2; /* dB */
-	const float RELEASE_TIME = 500; /* milliseconds */
-	const float RELEASE_THRESHOLD = -20; /* dB */
-	const float PRECISION = 1e-6f;
+    const float ATTACK_TIME = 5; /* milliseconds */
+    const float ATTACK_THRESHOLD = -2; /* dB */
+    const float RELEASE_TIME = 500; /* milliseconds */
+    const float RELEASE_THRESHOLD = -20; /* dB */
+    const float PRECISION = 1e-6f;
 
-	int nb_smp_attack = static_cast<int>(ATTACK_TIME / 1000.f * samplerate_);
-	int nb_smp_release = static_cast<int>(RELEASE_TIME / 1000.f * samplerate_);
+    int nb_smp_attack = static_cast<int>(ATTACK_TIME / 1000.f * samplerate_);
+    int nb_smp_release = static_cast<int>(RELEASE_TIME / 1000.f * samplerate_);
 
-	float attack_threshold =
-		static_cast<float>(std::pow(10, ATTACK_THRESHOLD / 20.f));
-	float release_threshold =
-		static_cast<float>(std::pow(10, RELEASE_THRESHOLD / 20.f));
+    float attack_threshold =
+        static_cast<float>(std::pow(10, ATTACK_THRESHOLD / 20.f));
+    float release_threshold =
+        static_cast<float>(std::pow(10, RELEASE_THRESHOLD / 20.f));
 
-	release_constant_ = std::pow(release_threshold, 1.f / nb_smp_release);
+    release_constant_ = std::pow(release_threshold, 1.f / nb_smp_release);
 
-	int smp = 0;
-	float current_value;
-	float qppm;
-	float attack_const = 0.f;
-	smp = 0;
-	while (nb_smp_attack > smp) {
-		smp = 0;
-		attack_const += PRECISION;
-		qppm = 0;
-		while (qppm <= attack_threshold && smp < nb_smp_attack + 1) {
-			current_value = static_cast<float>(std::fabs(
-				std::sin(2 * MathConstants<float>::pi * 5000.f * smp / samplerate_)));
-			if (current_value > qppm) {
-				qppm = current_value - attack_const * (current_value - qppm);
-			}
-			else {
-				qppm = qppm * release_constant_;
-			}
-			smp++;
-		}
-	}
-	attack_constant_ = attack_const;
+    int smp = 0;
+    float current_value;
+    float qppm;
+    float attack_const = 0.f;
+    smp = 0;
+    while(nb_smp_attack > smp) {
+        smp = 0;
+        attack_const += PRECISION;
+        qppm = 0;
+        while(qppm <= attack_threshold && smp < nb_smp_attack + 1) {
+            current_value = static_cast<float>(std::fabs(
+                std::sin(2 * MathConstants<float>::pi * 5000.f * smp / samplerate_)));
+            if(current_value > qppm) {
+                qppm = current_value - attack_const * (current_value - qppm);
+            } else {
+                qppm = qppm * release_constant_;
+            }
+            smp++;
+        }
+    }
+    attack_constant_ = attack_const;
 }
 
 }  // namespace plugin
--- shared/components/level_meter_calculator.hpp
+++ shared/components/level_meter_calculator.hpp
@@ -10,56 +10,56 @@ namespace plugin {
 
 class LevelMeterCalculator {
 public:
-	LevelMeterCalculator(std::size_t channels, std::size_t samplerate);
+    LevelMeterCalculator(std::size_t channels, std::size_t samplerate);
 
-	/// change setup
-	/**
-	 * Discards all measurements so far
-	 */
-	void setup(std::size_t channels, std::size_t samplerate);
+    /// change setup
+    /**
+     * Discards all measurements so far
+     */
+    void setup(std::size_t channels, std::size_t samplerate);
 
-	/// Process input samples
-	/**
-	 * Process all samples and update level values.
-	 *
-	 * @a input must have the correct channel count.
-	 */
-	void process(const AudioBuffer<float>& buffer);
+    /// Process input samples
+    /**
+     * Process all samples and update level values.
+     *
+     * @a input must have the correct channel count.
+     */
+    void process(const AudioBuffer<float>& buffer);
 
-	bool hasSignal(int channel);
-	bool thisTrackHasClipped();
-	bool thisChannelHasClipped(int channel);
-	//test
+    bool hasSignal(int channel);
+    bool thisTrackHasClipped();
+    bool thisChannelHasClipped(int channel);
+    //test
 
-	/// process zeros if the last measurement is more than blockPeriodLimitMs_
-	void decayIfNeeded();
+    /// process zeros if the last measurement is more than blockPeriodLimitMs_
+    void decayIfNeeded();
 
-	/// Get current level for a channel
-	float getLevel(std::size_t channel);
+    /// Get current level for a channel
+    float getLevel(std::size_t channel);
 
-	std::size_t samplerate() const { return samplerate_; }
-	std::size_t channels() const { return channels_; }
+    std::size_t samplerate() const { return samplerate_; }
+    std::size_t channels() const { return channels_; }
 
-	void resetClipping();
+    void resetClipping();
 
 private:
-	void processSample(float currentValue, std::size_t channel);
+    void processSample(float currentValue, std::size_t channel);
 
-	void setConstants();
-	void calcConstants();
+    void setConstants();
+    void calcConstants();
     std::size_t channels_{ 0 };
     std::size_t samplerate_{ 0 };
     std::size_t blocksize_{ 0 };
-	std::vector<float> lastLevel_;
-	float release_constant_;
-	float attack_constant_;
-	int64_t lastMeasurement_;
+    std::vector<float> lastLevel_;
+    float release_constant_;
+    float attack_constant_;
+    int64_t lastMeasurement_;
     int blockPeriodLimitMs_{ 60 };
 
-	std::vector<bool> lastLevelHasSignal_;
-	std::vector<bool> lastLevelHasClipped_;
+    std::vector<bool> lastLevelHasSignal_;
+    std::vector<bool> lastLevelHasClipped_;
 
-	std::mutex mutex_;
+    std::mutex mutex_;
 };
 
 }  // namespace plugin
