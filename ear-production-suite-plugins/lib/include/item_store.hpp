467312341673f85717d26e20ceeaf963d1c5a1f1
Richard Bailey <rsjtaylor.github@gmail.com>
2022-02-25 12:30:20 +0000

    Do ui thread dispatch in listener superclass rather than leaf classes

--- ear-production-suite-plugins/lib/include/item_store.hpp
+++ ear-production-suite-plugins/lib/include/item_store.hpp
@@ -18,136 +18,69 @@ namespace plugin {
 class ItemStore {
  public:
   class Listener {
-   public:
-    void itemAdded(proto::InputItemMetadata item) {
-      addItem(item);
-    }
-
-    void itemChanged(proto::InputItemMetadata oldItem,
-                     proto::InputItemMetadata newItem) {
-      changeItem(oldItem, newItem);
-    }
-
-    void itemRemoved(proto::InputItemMetadata oldItem) {
-      removeItem(oldItem);
-    }
    protected:
+    virtual void dispatch(std::function<void()> event);
     virtual void addItem(proto::InputItemMetadata const& item) = 0;
     virtual void changeItem(proto::InputItemMetadata const& oldItem,
                             proto::InputItemMetadata const& newItem) = 0;
 
     virtual void removeItem(proto::InputItemMetadata const& oldItem) = 0;
+    friend class ItemStore;
+   private:
+    void itemAdded(proto::InputItemMetadata const& item);
+    void itemChanged(proto::InputItemMetadata const& oldItem,
+                     proto::InputItemMetadata const& newItem);
+
+    void itemRemoved(proto::InputItemMetadata const& oldItem);
   };
 
+  [[ nodiscard ]] proto::InputItemMetadata get(communication::ConnectionId const& id) const;
+  std::optional<proto::InputItemMetadata> maybeGet(communication::ConnectionId const& id) const;
+  [[ nodiscard ]] std::map<communication::ConnectionId, proto::InputItemMetadata> allItems() const;
+  std::multimap<int, communication::ConnectionId> routeMap() const;
+
+
   // TODO remove this as setItem will do.
-  void addItem(communication::ConnectionId const& id) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    proto::InputItemMetadata emptyItemMetadata;
-    emptyItemMetadata.set_connection_id(id.string());
-    store_[id] = emptyItemMetadata;
-    notifyItemAdded(emptyItemMetadata);
-  }
+  void addItem(communication::ConnectionId const& id);
+  std::optional<proto::InputItemMetadata> setItem(communication::ConnectionId const& id, proto::InputItemMetadata const& item);
+  void removeItem(communication::ConnectionId const& id);
+  [[ nodiscard ]] bool clearChanged();
 
-  std::optional<proto::InputItemMetadata> setItem(communication::ConnectionId const& id, proto::InputItemMetadata const& item) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    std::optional<proto::InputItemMetadata> previousItem;
-    assert(id.string() == item.connection_id());
-    if (auto result = store_.emplace(id, item); result.second) {
-      // Currently this should never be called, but this path should be used
-      // when we remove addItem
-      assert(false);
-      notifyItemAdded(item);
-    } else {
-      previousItem = result.first->second;
-      result.first->second = item;
-      notifyItemChanged(result.first->second, item);
-    }
-    return previousItem;
-  }
-
-  void removeItem(communication::ConnectionId const& id) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    if(auto it = store_.find(id); it != store_.end()) {
-      auto item = it->second;
-      store_.erase(it);
-      notifyItemRemoved(item);
-    }
-  }
-
-  [[ nodiscard ]]
-  proto::InputItemMetadata get(communication::ConnectionId const& id) const {
-    std::lock_guard<std::mutex> lock(mutex_);
-    return store_.at(id);
-  }
-
-  std::optional<proto::InputItemMetadata> maybeGet(communication::ConnectionId const& id) const {
-    std::lock_guard<std::mutex> lock(mutex_);
-    std::optional<proto::InputItemMetadata> item;
-    if(auto it = store_.find(id); it != store_.end()) {
-      item = it->second;
-    }
-    return item;
-  }
-
-  [[ nodiscard ]]
-  std::map<communication::ConnectionId, proto::InputItemMetadata> allItems() const {
-    std::lock_guard<std::mutex> lock(mutex_);
-    return store_;
-  }
-
-  [[ nodiscard ]]
-  bool clearChanged() {
-    std::lock_guard<std::mutex> lock(mutex_);
-    bool clearedAny{false};
-    for(auto& item : store_) {
-      if(item.second.changed()) {
-        item.second.set_changed(false);
-        clearedAny = true;
-      }
-    }
-    return clearedAny;
-  }
-
-  std::multimap<int, communication::ConnectionId> routeMap() const {
-    std::lock_guard<std::mutex> lock(mutex_);
-    std::multimap<int, communication::ConnectionId> routes;
-    std::transform(store_.cbegin(), store_.cend(),
-                   std::inserter(routes, routes.begin()),
-                   [](auto const& idItemPair) {
-                     return std::make_pair(idItemPair.second.routing(),
-                                           idItemPair.first);
-                   });
-    return routes;
-  }
-
-  void addListener(std::shared_ptr<Listener> const& listener) {
-    listeners_.emplace_back(listener);
-  }
+  void addListener(std::shared_ptr<Listener> const& listener);
 
  private:
   mutable std::mutex mutex_;
   std::vector<std::weak_ptr<Listener>> listeners_;
   std::map<communication::ConnectionId, proto::InputItemMetadata> store_;
 
+  // note deliberate capture by value in inner lambda, this is so
+  // we can dispatch asynchronously without the listener expiring or
+  // synchronisation issues with the item store data
   void notifyItemAdded(proto::InputItemMetadata const& item) {
     removeDeadPointersAfter(listeners_,
-                            [&item](auto const& listener) {
-                              listener->itemAdded(item);
+                            [&item, this](auto const& listener) {
+                              listener->dispatch([item, listener]{
+                                listener->itemAdded(item);
+                              });
                             });
   }
 
   void notifyItemChanged(proto::InputItemMetadata const& newItem,
                    proto::InputItemMetadata const& oldItem) {
     removeDeadPointersAfter(listeners_,
-                            [&newItem, &oldItem](auto const& listener) {
-                              listener->itemChanged(newItem, oldItem);
+                            [&oldItem, &newItem, this](auto const& listener) {
+                              listener->dispatch([oldItem, newItem, listener]{
+                                listener->itemChanged(oldItem, newItem);
+                              });
                             });
   }
 
   void notifyItemRemoved(proto::InputItemMetadata const& oldItem) {
     removeDeadPointersAfter(listeners_,
-                            [&oldItem](auto const& listener) {
-                              listener->itemRemoved(oldItem);
+                            [&oldItem, this](auto const& listener) {
+                              listener->dispatch([oldItem, listener]{
+                                listener->itemRemoved(oldItem);
+                              });
                             });
   }
 
---
+++ ear-production-suite-plugins/plugins/scene/src/ui_item_store_listener.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Richard Bailey on 25/02/2022.
+//
+
+#include "ui_item_store_listener.hpp"
