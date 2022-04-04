136145a7f745c01756a2ef1f20fdc29c480a9182
Matt Firth <matt.firth@bbc.co.uk>
2022-04-04 22:53:04 +0100

    Implement improved metadata sender pattern for HOA

--- ear-production-suite-plugins/lib/include/communication/hoa_metadata_sender.hpp
+++ ear-production-suite-plugins/lib/include/communication/hoa_metadata_sender.hpp
@@ -1,15 +1,11 @@
 #pragma once
 
-#include "common_types.hpp"
-#include "message_buffer.hpp"
-#include "nng-cpp/nng.hpp"
-#include "log.hpp"
-#include "input_item_metadata.pb.h"
-#include <boost/optional.hpp>
 #include <string>
 #include <chrono>
-#include <mutex>
-#include <atomic>
+
+#include "metadata_sender.hpp"
+#include "data_wrapper.hpp"
+#include "log.hpp"
 
 namespace ear {
 namespace plugin {
@@ -18,13 +14,9 @@ namespace communication {
 class HoaMetadataSender {
  public:
   HoaMetadataSender(std::shared_ptr<spdlog::logger> logger = nullptr);
-  ~HoaMetadataSender();
-
   void logger(std::shared_ptr<spdlog::logger> logger);
-
   void connect(const std::string& endpoint, ConnectionId connectionId);
   void disconnect();
-
   void triggerSend();
 
   void routing(int32_t value);
@@ -32,26 +24,16 @@ class HoaMetadataSender {
   void colour(int value);
   void packFormatIdValue(int value);
 
-  ConnectionId getConnectionId() { return connectionId_; }
+  ConnectionId getConnectionId() { return sender_.connectionId(); }
 
  private:
-  void sendMetadata();
-  void startTimer();
-  MessageBuffer getMessage();
-  void handleTimeout(std::error_code ec);
-
+   template<typename FunctionT>
+   void setData(FunctionT&& set) {
+     data_.writeAccess([set](auto data) { std::invoke(set, data); });
+   }
   std::shared_ptr<spdlog::logger> logger_;
-  nng::PushSocket socket_;
-  nng::AsyncIO timer_;
-  nng::Dialer dialer_;
-  std::mutex dataMutex_;
-  std::mutex timeoutMutex_;
-  std::mutex sendMutex_;
-  proto::InputItemMetadata data_;
-  ConnectionId connectionId_;
-  std::chrono::system_clock::time_point lastSendTimestamp_;
-  std::chrono::milliseconds maxSendInterval_;
-  std::atomic<bool> timerRunning{false};
+  DataWrapper data_;
+  MetadataSender sender_;
 };
 
 }  // namespace communication
