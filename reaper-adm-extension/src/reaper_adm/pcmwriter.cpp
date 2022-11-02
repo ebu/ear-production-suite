5a16e793c126d7f865202bc9caea643dacb6b662
Matt Firth <matt.firth@bbc.co.uk>
2022-11-02 12:34:51 +0000

    Force use of RF64 ID on assets to be reimported (#218)
    * Use RF64 ID on assets >4GB to be reimported
    
    * Use libbw64 main repo after feature merge

--- reaper-adm-extension/src/reaper_adm/pcmwriter.cpp
+++ reaper-adm-extension/src/reaper_adm/pcmwriter.cpp
@@ -15,6 +15,7 @@ void PCMWriter::write(const IPCMBlock &block)
 {
     if(!writer) {
         writer = bw64::writeFile(fileName().c_str(), block.channelCount(), block.sampleRate(), 24);
+        writer->useRf64Id(true); // REAPER currently doesn't recognise BW64 chunk ID when we pull on to track
     }
     auto framesWritten = writer->write(&block.data()[0], block.frameCount());
 }
