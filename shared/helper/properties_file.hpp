5bc90957298eefa90ca6a05a8fdb5d580118f244
Matt Firth <matt.firth@bbc.co.uk>
2023-02-16 18:44:25 +0000

    Use a common folder for logs, properties files and settings
    Make sure it's in Application Support for macOS too.

--- shared/helper/properties_file.hpp
+++ shared/helper/properties_file.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "JuceHeader.h"
+#include "resource_paths_juce-file.hpp"
 
 inline std::unique_ptr<PropertiesFile> getPropertiesFile(
     InterProcessLock* lock) {
   PropertiesFile::Options options;
-  options.applicationName = String("EAR Production Suite");
-  options.filenameSuffix = String("xml");
-  options.folderName = String("EBU");
-  options.osxLibrarySubFolder = String("Application Support");
+  options.applicationName = String("AllPlugins");
+  options.filenameSuffix = String("settings");
+  options.folderName = ResourcePaths::getSettingsDirectory(true).getFullPathName();
   options.processLock = lock;
   return std::make_unique<PropertiesFile>(options);
 }
