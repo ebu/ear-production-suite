47cd1e16f43d0eeacf55112e32ffa4b635103126
Matt Firth <matt.firth@bbc.co.uk>
2023-02-21 23:49:06 +0000

    Only include necessary juce modules

--- reaper-adm-extension/src/reaper_adm/AppConfig.h
+++ reaper-adm-extension/src/reaper_adm/AppConfig.h
@@ -15,18 +15,18 @@
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_MODULE_AVAILABLE_juce_audio_basics          1
-#define JUCE_MODULE_AVAILABLE_juce_audio_devices         1
-#define JUCE_MODULE_AVAILABLE_juce_audio_formats         1
-#define JUCE_MODULE_AVAILABLE_juce_audio_processors      1
 #define JUCE_MODULE_AVAILABLE_juce_core                  1
-#define JUCE_MODULE_AVAILABLE_juce_cryptography          1
-#define JUCE_MODULE_AVAILABLE_juce_data_structures       1
-#define JUCE_MODULE_AVAILABLE_juce_events                1
-#define JUCE_MODULE_AVAILABLE_juce_graphics              1
-#define JUCE_MODULE_AVAILABLE_juce_gui_basics            1
-#define JUCE_MODULE_AVAILABLE_juce_gui_extra             1
-#define JUCE_MODULE_AVAILABLE_juce_opengl                1
+#define JUCE_MODULE_AVAILABLE_juce_audio_basics          0
+#define JUCE_MODULE_AVAILABLE_juce_audio_devices         0
+#define JUCE_MODULE_AVAILABLE_juce_audio_formats         0
+#define JUCE_MODULE_AVAILABLE_juce_audio_processors      0
+#define JUCE_MODULE_AVAILABLE_juce_cryptography          0
+#define JUCE_MODULE_AVAILABLE_juce_data_structures       0
+#define JUCE_MODULE_AVAILABLE_juce_events                0
+#define JUCE_MODULE_AVAILABLE_juce_graphics              0
+#define JUCE_MODULE_AVAILABLE_juce_gui_basics            0
+#define JUCE_MODULE_AVAILABLE_juce_gui_extra             0
+#define JUCE_MODULE_AVAILABLE_juce_opengl                0
 
 #define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED 1
 
