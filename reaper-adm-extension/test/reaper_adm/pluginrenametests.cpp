9dadc77f06c34b5d9490c2d38bf3f910f83cbf9c
Matt Firth <matt.firth@bbc.co.uk>
2023-03-17 18:53:41 +0000

    Tests for CleanFXName

--- reaper-adm-extension/test/reaper_adm/pluginrenametests.cpp
+++ reaper-adm-extension/test/reaper_adm/pluginrenametests.cpp
@@ -124,3 +124,27 @@ TEST_CASE("Track State Chunk Parsing") {
     }
 
 }
+
+TEST_CASE("Clean Plugin Name") {
+
+    reaper_plugin_info_t info{};
+    admplug::ReaperAPIImpl api(info);
+
+    SECTION("Containing only developer segment") {
+        std::string name = "VST3: EAR Object (EBU)";
+        api.CleanFXName(name);
+        REQUIRE(name == "EAR Object");
+    }
+
+    SECTION("Containing developer segment and channel count") {
+        std::string name = "VST3: EAR Monitoring 0+2+0 (EBU) (64ch)";
+        api.CleanFXName(name);
+        REQUIRE(name == "EAR Monitoring 0+2+0");
+    }
+
+    SECTION("Already Clean") {
+        std::string name = "EAR Object";
+        api.CleanFXName(name);
+        REQUIRE(name == "EAR Object");
+    }
+}
