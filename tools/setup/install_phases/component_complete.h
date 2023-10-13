8f9ca6c521dfdfe47ce7c9c840ac1afa70481abc
Matt Firth <matt.firth@bbc.co.uk>
2023-10-13 17:58:10 +0100

    AutoUpdateCheckButton component in final setup screen

--- tools/setup/install_phases/component_complete.h
+++ tools/setup/install_phases/component_complete.h
@@ -1,6 +1,35 @@
 #pragma once
 
 #include "JuceHeader.h"
+#include <memory>
+
+class AutoUpdateCheckButton : public Component
+{
+public:
+    AutoUpdateCheckButton();
+    ~AutoUpdateCheckButton();
+
+    void paint(Graphics&) override;
+    void resized() override;
+
+    int getMinReqWidth(int forComponentHeight);
+
+    void setState(bool checked);
+
+    void mouseDown(const MouseEvent& event) override;
+
+private:
+    Label text1;
+    Label text2;
+
+    const String text1Str{ "Automatically check for updates on start-up (requires internet connection.)" };
+    const String text2Str{ "You can change your preference at any time from the REAPER Extensions menu." };
+    const int marginBoxText{ 10 };
+
+    bool enabled{ false };
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AutoUpdateCheckButton)
+};
 
 class ComponentComplete : public Component
 {
@@ -19,6 +48,7 @@ private:
     Label titleLabel;
     Label descriptionLabel;
     TextButton exitButton;
+    std::unique_ptr<AutoUpdateCheckButton> autoUpdateCheck;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ComponentComplete)
 };
