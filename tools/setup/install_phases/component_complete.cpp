c705dc08c096edbc88564cabff58d5838a907bdd
Matt Firth <matt.firth@bbc.co.uk>
2023-10-13 17:58:10 +0100

    AutoUpdateCheckButton component in final setup screen

--- tools/setup/install_phases/component_complete.cpp
+++ tools/setup/install_phases/component_complete.cpp
@@ -39,7 +39,15 @@ void ComponentComplete::resized()
     auto sectionHeight = area.getHeight() / 4;
 
     titleLabel.setBounds(area.removeFromTop(sectionHeight));
-    descriptionLabel.setBounds(area.removeFromTop(sectionHeight));
+    descriptionLabel.setBounds(area.removeFromTop(sectionHeight / 2));
+
+    if (autoUpdateCheck) {
+        const int autoUpdateCheckHeight{ 35 };
+        auto autoUpdateCheckWidth = autoUpdateCheck->getMinReqWidth(autoUpdateCheckHeight);
+        auto autoUpdateCheckArea = area.removeFromTop(sectionHeight / 2).withSizeKeepingCentre(autoUpdateCheckWidth, autoUpdateCheckHeight);
+        if (autoUpdateCheckArea.getX() < 0) autoUpdateCheckArea.setX(0);
+        autoUpdateCheck->setBounds(autoUpdateCheckArea);
+    }
 
     auto buttonSectionWidth = area.getWidth();
     auto buttonHeight = std::min(area.getHeight(), 40);
@@ -51,6 +59,10 @@ void ComponentComplete::resized()
 
 void ComponentComplete::configureForUninstallPhase()
 {
+    if (autoUpdateCheck) {
+        removeChildComponent(autoUpdateCheck.get());
+        autoUpdateCheck.reset();
+    }
     titleLabel.setText("Uninstall Complete",
         juce::NotificationType::dontSendNotification);
     descriptionLabel.setText("Uninstallation is complete.",
@@ -59,6 +71,10 @@ void ComponentComplete::configureForUninstallPhase()
 
 void ComponentComplete::configureForUninstallUnnecessaryPhase()
 {
+    if (autoUpdateCheck) {
+        removeChildComponent(autoUpdateCheck.get());
+        autoUpdateCheck.reset();
+    }
     titleLabel.setText("Uninstall",
         juce::NotificationType::dontSendNotification);
     descriptionLabel.setText("Setup did not find any files relating to a previous installation.",
@@ -67,8 +83,70 @@ void ComponentComplete::configureForUninstallUnnecessaryPhase()
 
 void ComponentComplete::configureForInstallPhase()
 {
+    if (!autoUpdateCheck) {
+        autoUpdateCheck = std::make_unique<AutoUpdateCheckButton>();
+        addAndMakeVisible(autoUpdateCheck.get());
+    }
     titleLabel.setText("Installation Complete",
         juce::NotificationType::dontSendNotification);
     descriptionLabel.setText("The EAR Production Suite has been installed.\nUse the software via the REAPER digital audio workstation.",
         juce::NotificationType::dontSendNotification);
+}
+
+AutoUpdateCheckButton::AutoUpdateCheckButton()
+{
+    text1.setFont(EarFontsSingleton::instance().Label);
+    text1.setColour(Label::textColourId, EarColours::Label.withAlpha(Emphasis::high));
+    text1.setJustificationType(Justification::centredLeft);
+    text1.setText(text1Str, dontSendNotification);
+    addAndMakeVisible(text1);
+
+    text2.setFont(EarFontsSingleton::instance().Label);
+    text2.setColour(Label::textColourId, EarColours::Label.withAlpha(Emphasis::medium));
+    text2.setJustificationType(Justification::centredLeft);
+    text2.setText(text2Str, dontSendNotification);
+    addAndMakeVisible(text2);
+
+    this->setMouseCursor(MouseCursor::PointingHandCursor);
+    text1.setMouseCursor(MouseCursor::PointingHandCursor);
+    text2.setMouseCursor(MouseCursor::PointingHandCursor);
+    text1.setInterceptsMouseClicks(false, false);
+    text2.setInterceptsMouseClicks(false, false);
+}
+
+AutoUpdateCheckButton::~AutoUpdateCheckButton()
+{
+}
+
+void AutoUpdateCheckButton::paint(Graphics& g)
+{
+    auto area = getLocalBounds();
+    getLookAndFeel().drawTickBox(g, text1, 1, 1, area.getHeight() - 2, area.getHeight() - 2, enabled, true, true, false);
+}
+
+void AutoUpdateCheckButton::resized()
+{
+    auto area = getLocalBounds();
+    area.removeFromLeft(area.getHeight() + 10);
+    text1.setBounds(area.removeFromTop(area.getHeight() / 2));
+    text2.setBounds(area);
+}
+
+int AutoUpdateCheckButton::getMinReqWidth(int forComponentHeight)
+{
+    auto text1Width = EarFontsSingleton::instance().Label.getStringWidthFloat(text1Str);
+    auto text2Width = EarFontsSingleton::instance().Label.getStringWidthFloat(text2Str);
+    auto textWidth = std::max(text1Width, text2Width) * 1.05f; // 5% excess
+    return forComponentHeight + marginBoxText + textWidth;
+}
+
+void AutoUpdateCheckButton::setState(bool checked)
+{
+    enabled = checked;
+    repaint();
+}
+
+void AutoUpdateCheckButton::mouseDown(const MouseEvent& event)
+{
+    setState(!enabled);
 }
