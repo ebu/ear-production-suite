bbd15818c449218091b881b6878144b624766d11
Matt Firth <matt.firth@bbc.co.uk>
2023-10-23 20:02:06 +0100

    Simpler lookup for legacy DS index to PF ID
    Working towards removing the horrible hard-coded speaker setups and using indexes to refer to them

--- shared/speaker_setups.hpp
+++ shared/speaker_setups.hpp
@@ -404,16 +404,6 @@ speakerSetupByPackFormatId(const std::string &packFormatId) {
   }
 }
 
-inline int getIndexFromLegacySpeakerSetupIndex(int legacyIndex) {
-  for(std::size_t i = 0; i != SPEAKER_SETUPS.size(); ++i) {
-    auto candidateIndex = SPEAKER_SETUPS[i].legacySpeakerSetupIndex;
-    if(candidateIndex && *candidateIndex == legacyIndex) {
-      return static_cast<int>(i);
-    }
-  }
-  return -1;
-}
-
 inline int getIndexFromPackFormatIdValue(int packFormatIdValue) {
   for(int i = 0; i < SPEAKER_SETUPS.size(); i++) {
     if(SPEAKER_SETUPS[i].packFormatIdValue == packFormatIdValue) {
@@ -435,6 +425,44 @@ inline int getIndexFromPackFormatId(std::string packFormatId) {
 inline int getChannelCountFromSpeakerSetupIndex(int index) {
     if(index < 0 || index >= SPEAKER_SETUPS.size()) return -1;
     return SPEAKER_SETUPS[index].speakers.size();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+inline uint16_t getPackFormatIdValueFromLegacySpeakerSetupsIndex(int index) {
+    /* EPS prior to version 0.7.0 used a speaker_setup_index property rather 
+       rather than a packformat ID to refer to a specific DS PF */
+    const std::vector<int> oldIdOrdering{
+        1,  // 0 = AP_00010001
+        2,  // 1 = AP_00010002
+        3,  // 2 = AP_00010003
+        4,  // 3 = AP_00010004
+        5,  // 4 = AP_00010005
+        16, // 5 = AP_00010010
+        7,  // 6 = AP_00010007
+        8,  // 7 = AP_00010008
+        9,  // 8 = AP_00010009
+        15, // 9 = AP_0001000f
+        23, // 10 = AP_00010017
+        22, // 11 = AP_00010016
+        10, // 12 = AP_0001000a
+        12  // 13 = AP_0001000c
+    };
+    if (index >= 0 && index < oldIdOrdering.size()) {
+        return oldIdOrdering[index];
+    }
+    return 0; // Invalid ID used throughout for "unset"
 }
 
 }  // namespace plugin
