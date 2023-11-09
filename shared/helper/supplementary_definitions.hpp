2732d3be7b5424b265e75a8a301a0d768d502d2e
Matt Firth <matt.firth@bbc.co.uk>
2023-11-09 16:15:15 +0000

    Pull in supplementary defs to helper

---
+++ shared/helper/supplementary_definitions.hpp
@@ -0,0 +1,116 @@
+#include <string>
+
+const std::string xmlSupplementaryDefinitions =
+R"5UPPL3M3N74RY(
+
+<?xml version='1.0' encoding='utf-8'?>
+<ituADM xmlns="urn:metadata-schema:adm">
+  <coreMetadata>
+    <format>
+      <audioFormatExtended>
+        <audioPackFormat audioPackFormatID="AP_00011101" audioPackFormatName="3.0 Surr" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010009</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011102" audioPackFormatName="5.0 + 2H" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010005</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010006</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000F</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011103" audioPackFormatName="5.1 + 6H" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010004</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010005</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010006</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000F</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000E</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010010</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010012</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000C</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011104" audioPackFormatName="7.0 + 2H" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000A</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000B</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001C</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010013</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010014</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011105" audioPackFormatName="7.0 (90 deg)" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001C</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000A</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000B</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011106" audioPackFormatName="7.1 + 4H" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010004</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001C</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000A</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000B</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000F</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001E</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001F</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011107" audioPackFormatName="7.1 (60 deg)" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010004</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010005</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010006</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010018</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010019</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011108" audioPackFormatName="7.1 (110 deg)" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010003</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010004</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010005</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010006</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001C</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001D</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_00011109" audioPackFormatName="Quad" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_00010001</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010002</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010005</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010006</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_0001110a" audioPackFormatName="Quad Height 110" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_0001000D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000F</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010010</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_00010012</audioChannelFormatIDRef>
+        </audioPackFormat>
+        <audioPackFormat audioPackFormatID="AP_0001110b" audioPackFormatName="Quad Height 135" typeLabel="0001">
+          <audioChannelFormatIDRef>AC_0001000D</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001000F</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001E</audioChannelFormatIDRef>
+          <audioChannelFormatIDRef>AC_0001001F</audioChannelFormatIDRef>
+        </audioPackFormat>
+      </audioFormatExtended>
+    </format>
+  </coreMetadata>
+</ituADM>
+
+)5UPPL3M3N74RY";
