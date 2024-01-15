c92f7b768cc25e9b5a9db45770e6d37108688e78
Matt Firth <matt.firth@bbc.co.uk>
2024-01-15 12:44:03 +0000

    Avoid creating new insts of DataFile in updateAvailableFiles
    ...when existing entry available and details unchanged

--- ear-production-suite-plugins/plugins/binaural_monitoring/src/bear_data_files.cpp
+++ ear-production-suite-plugins/plugins/binaural_monitoring/src/bear_data_files.cpp
@@ -1,6 +1,14 @@
 #include "bear_data_files.hpp"
 #include <bear/api.hpp>
 
+namespace {
+bool operator==(const ear::plugin::DataFileManager::DataFile& lhs,
+                const ear::plugin::DataFileManager::DataFile& rhs) {
+  return (lhs.filename == rhs.filename) && (lhs.fullPath == rhs.fullPath) &&
+         (lhs.isBearRelease == rhs.isBearRelease) && (lhs.label == rhs.label);
+}
+}
+
 namespace ear {
 namespace plugin {
 
@@ -10,24 +18,34 @@ DataFileManager::DataFileManager(const juce::File& path) {
 }
 
 void DataFileManager::updateAvailableFiles() {
-  availableDataFiles_.clear();
+  // Start a new vec and copy existing shared_ptrs of unchanged files 
+  // This retains shared_ptrs unlike clear() and rebuild
+  std::vector<std::shared_ptr<DataFileManager::DataFile>> dfs;
   auto files = path_.findChildFiles(juce::File::TypesOfFileToFind::findFiles,
                                     false, "*.tf");
   for (const auto& file : files) {
-    auto df = std::make_shared<DataFile>();
-    df->fullPath = file;
-    df->filename = file.getFileName();
+    auto newDf = std::make_shared<DataFile>();
+    newDf->fullPath = file;
+    newDf->filename = file.getFileName();
     try {
       auto md = bear::DataFileMetadata::read_from_file(
           file.getFullPathName().toStdString());
-       if(md.has_metadata()) {
-        df->label = md.get_label();
-        df->isBearRelease = md.is_released();
+      if (md.has_metadata()) {
+        newDf->label = md.get_label();
+        newDf->isBearRelease = md.is_released();
       }
     } catch (std::exception) {
     }
-    availableDataFiles_.push_back(df);
+    // Use the existing struct where possible (exists and is identical)
+    // -- this retains the same inst and shared_ptr
+    auto existingDf = getDataFileInfo(file.getFileName());
+    if (existingDf && *existingDf == *newDf) {
+      dfs.push_back(existingDf);
+    } else {
+      dfs.push_back(newDf);
+    }
   }
+  availableDataFiles_ = dfs;
 }
 
 std::shared_ptr<DataFileManager::DataFile>
