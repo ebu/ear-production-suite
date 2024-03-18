ce3b8971787c6d24ab244150a4c0cfb7e515805e
Matt Firth <matt.firth@bbc.co.uk>
2024-03-18 10:46:21 +0000

    Codesigning status

--- packaging/codesign/codesign.py
+++ packaging/codesign/codesign.py
@@ -3,8 +3,12 @@ import subprocess
 import os
 from glob import glob
 
+def status(message):
+    print("\n>>>>>>>>>> " + message + "\n")
+
 def sign(certificate):
     # Sign all plugins
+    status("Signing plugins...")
     plugins = glob(os.path.join("tmp/VST3/ear-production-suite", "*.vst3"))
     plugins.append("tmp/VST3/ADM Export Source.vst3")
     for p in plugins:
@@ -15,20 +19,24 @@ def sign(certificate):
                     "-s", certificate, "tmp/Setup EAR Production Suite.app"])
                     
     # Sign project upgrade gui app bundle
+    status("Signing project upgrade gui app...")
     subprocess.run(["xcrun", "codesign", "--timestamp", "--options", "runtime",
                     "-s", certificate, "tmp/Tools/Project Upgrade Utility GUI.app"])
 
     # REAPER extension is naked dylib so no Info.plist to derive identifier from, specify on command line
+    status("Signing reaper extension...")
     subprocess.run(["xcrun", "codesign", "--timestamp", "--options", "runtime",
                     "-i", "ch.ebu.eps.reaper_adm",
                     "-s", certificate, "tmp/UserPlugins/reaper_adm.dylib"])
 
     # Project upgrade command line exe has no Info.plist to derive identifier from, specify on command line
+    status("Signing project upgrade cli tool...")
     subprocess.run(["xcrun", "codesign", "--timestamp", "--options", "runtime",
                     "-i", "ch.ebu.eps.reaper_project_upgrade",
                     "-s", certificate, "tmp/Tools/project_upgrade"])
 
 def extract(fileName):
+    status("Extracting...")
     subprocess.run(["tar", "-xvf", fileName])
     tarFile = fileName.replace("zip", "tar")
     subprocess.run(["rm", "-rf", "tmp/"])
@@ -37,11 +45,13 @@ def extract(fileName):
     subprocess.run(["rm", tarFile])
 
 def createDmg(outputname):
+    status("Creating disk image...")
     dmgFile = outputname + ".dmg"
     volName = outputname
     subprocess.run(["hdiutil", "create", "-volname", volName, "-srcfolder", "./tmp", "-ov", "-format", "UDZO", dmgFile])
     
 def signDmg(certificate, outputname):
+    status("Signing disk image...")
     dmgFile = outputname + ".dmg"
     subprocess.run(["xcrun", "codesign", "-s", certificate, dmgFile])
 
